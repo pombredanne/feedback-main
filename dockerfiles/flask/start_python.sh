ee62b41a6698af6b2c5c945dc7be6553479944db
erwanledoux <lawanledoux@gmail.com>
2018-10-16 23:59:33 +0200

    adde sandbox init

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -3,6 +3,7 @@ set -x
 cd /opt/services/flaskapp/src
 pip install -r requirements.txt
 python -m nltk.downloader punkt
+python scripts/sf.py sandbox --name=init
 sleep 10 # This leaves time for Postgres to create the DB
 while true; do
     python app.py
