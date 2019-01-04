57ce97c447fac5396e04f6b48496562183c05fdd
erwanledoux <lawanledoux@gmail.com>
2019-01-04 17:06:17 +0100

    fixed APP_NAME cmd

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -2,7 +2,6 @@
 set -x
 cd /opt/services/flaskapp/src
 pip install -r requirements.txt
-#python -m nltk.downloader punkt
 sleep 10 # This leaves time for Postgres to create the DB
 while true; do
     python app.py
