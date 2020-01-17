958399ec0fa86d270d45611a9f8c3e39daa3bae0
Colas Kerkhove <colas.kerkhove@gmail.com>
2020-01-17 17:44:23 +0100

    Create a verdict for each article in sandbox

--- api/sandboxes/scripts/creators/ci/create_verdicts.py
+++ api/sandboxes/scripts/creators/ci/create_verdicts.py
@@ -10,18 +10,17 @@ def create_verdicts():
     logger.info('create_verdicts')
 
     verdicts_by_name = {}
-
-    article = Article.query\
-                     .filter_by(url="http://www.dailymail.co.uk/sciencetech/article-4192182/World-leaders-duped-manipulated-global-warming-data.html").one()
+    articles = Article.query.all()
     evaluation = Evaluation.query.filter_by(type="article", value=1).one()
     user = User.query.filter_by(email="sftest.editor.0@sciencefeedback.co").one()
-    verdicts_by_name["Great Barrier / editor 0"] = create_verdict(
-        user,
-        article,
-        "C'est neutre en fin de compte",
-        evaluation=evaluation,
-        rating=1,
-    )
+    for i, article in enumerate(articles):
+        verdicts_by_name[article.id] = create_verdict(
+            user,
+            article,
+            "C'est neutre en fin de compte %d" % i,
+            evaluation=evaluation,
+            rating=1,
+        )
 
     ApiHandler.save(*verdicts_by_name.values())
 
