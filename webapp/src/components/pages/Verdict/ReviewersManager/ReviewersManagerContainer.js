6abde75259563fd588020bf7487852572e777285
Colas Kerkhove <colas.kerkhove@gmail.com>
2020-01-17 18:13:32 +0100

    Fix reviews selector

--- webapp/src/components/pages/Verdict/ReviewersManager/ReviewersManagerContainer.js
+++ webapp/src/components/pages/Verdict/ReviewersManager/ReviewersManagerContainer.js
@@ -2,16 +2,21 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { compose } from 'redux'
 
-import selectReviewsByVerdictId from 'selectors/selectReviewsByVerdictId'
+import selectVerdictById from 'selectors/selectVerdictById'
+import selectReviewsByArticleId from 'selectors/selectReviewsByArticleId'
 
 import ReviewersManager from './ReviewersManager'
 import selectUsersByVerdictId from '../../../../selectors/selectUsersByVerdictId'
 
 function mapStateToProps (state, ownProps) {
   const { match: { params: { verdictId } } } = ownProps
+  const verdictUsers = selectUsersByVerdictId(state, verdictId)
+  const verdict = selectVerdictById(state, verdictId)
+  const { articleId } = verdict || {}
+  const reviews = selectReviewsByArticleId(state, articleId)
   return {
-    verdictUsers: selectUsersByVerdictId(state, verdictId),
-    reviews: selectReviewsByVerdictId(state, verdictId)
+    verdictUsers,
+    reviews: reviews
   }
 }
 
