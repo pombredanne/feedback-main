d1859fc312c3524bedd7207317e732eded85e5a8
Eledoux <lawanledoux@gmail.com>
2020-02-16 14:50:14 +0100

    added publication and user_publication migration

---
+++ api/alembic/versions/868064337798_add_publication_and_user_publication.py
@@ -0,0 +1,38 @@
+"""Add publication and user_publication
+
+Revision ID: 868064337798
+Revises: 34b241d9501a
+Create Date: 2020-02-16 13:44:42.067784
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '868064337798'
+down_revision = '34b241d9501a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'publication',
+        sa.Column('id', sa.BigInteger(), primary_key=True, autoincrement=True),
+        sa.Column('summary', sa.Text()),
+        sa.Column('tags', sa.Text()),
+        sa.Column('url', sa.String(220), nullable=False, unique=True)
+    )
+
+    op.create_table(
+        'user_publication',
+        sa.Column('id', sa.BigInteger(), primary_key=True, autoincrement=True),
+        sa.Column('publicationId', sa.BigInteger(), nullable=False),
+        sa.Column('userId', sa.BigInteger(), nullable=False)
+    )
+
+
+def downgrade():
+    op.drop_table('user_publication')
+    op.drop_table('publication')
