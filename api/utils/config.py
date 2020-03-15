f224168e6c768eb4cf8fdd5943b9029384ec8835
Eledoux <lawanledoux@gmail.com>
2020-03-15 03:01:34 +0100

    adapted config api

--- api/utils/config.py
+++ api/utils/config.py
@@ -4,22 +4,29 @@ from pathlib import Path
 
 APP_NAME = os.environ.get('APP_NAME', '')
 COMMAND_NAME = os.environ.get('COMMAND_NAME', '')
-GIT_TAG = os.environ.get('GIT_TAG', '')
+MODE = os.environ.get('MODE', 'serve-development')
 TLD = os.environ.get('TLD', '')
+WEBAPP_SUBDOMAIN = os.environ.get('WEBAPP_SUBDOMAIN', '')
 
 EMAIL_HOST = "{}.{}".format(APP_NAME, TLD)
 API_ROOT_PATH = Path(os.path.dirname(os.path.realpath(__file__))) / '..'
-BROWSER_URL = os.environ.get('BROWSER_URL', 'http://localhost:3000')
-ENV = os.environ.get('ENV', 'development')
-IS_DEVELOPMENT = ENV == 'development'
-IS_INTEGRATION = ENV == 'integration'
-IS_STAGING = ENV == 'staging'
-IS_PRODUCTION = ENV == 'production'
+
+version_file = open(os.path.join(API_ROOT_PATH, "version.txt"), "r")
+VERSION = version_file.read().rstrip()
+version_file.close()
+
+MACHINE_ENV = os.environ.get('MACHINE_ENV', 'development')
+IS_DEVELOPMENT = MACHINE_ENV == 'development'
+IS_PRODUCTION = MACHINE_ENV == 'production'
+
 LOG_LEVEL = int(os.environ.get('LOG_LEVEL', LOG_LEVEL_INFO))
 
 if IS_DEVELOPMENT:
-    API_URL = 'localhost'
+    API_URL = 'http://localhost:80'
+    WEBAPP_URL = 'http://localhost:3000'
 elif IS_PRODUCTION:
-    API_URL = 'https://backend.{}.{}'.format(APP_NAME, TLD)
+    API_URL = 'https://api.{}.{}'.format(APP_NAME, TLD)
+    WEBAPP_URL = 'https://{}.{}.{}'.format(WEBAPP_SUBDOMAIN, APP_NAME, TLD)
 else:
-    API_URL = 'https://backend-{}.{}.{}'.format(ENV, APP_NAME, TLD)
+    API_URL = 'https://api-{}.{}.{}'.format(MACHINE_ENV, APP_NAME, TLD)
+    WEBAPP_URL = 'https://{}.{}.{}'.format(WEBAPP_SUBDOMAIN, APP_NAME, TLD)
