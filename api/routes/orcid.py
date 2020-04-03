eb86ed17780d9e1e588622f14e7c63a2f67c5d5f
heloisethero <thero.heloise@gmail.com>
2020-04-03 18:43:44 +0200

    try to display the publication doi urls from the orcid

--- api/routes/orcid.py
+++ api/routes/orcid.py
@@ -8,5 +8,9 @@ def get_orcid(orcid_id):
     
     content = record_from_orcid_id(orcid_id)
     # content = { "publication1": "foo" }
+    # content = {
+    #     'publication1': 'https://doi.org/10.1038/s41562-019-0681-8', 
+    #     'publication2': 'https://doi.org/10.1038/s41467-018-06781-2'
+    #     }
 
     return jsonify(content)
